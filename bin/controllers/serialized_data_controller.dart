adf6bc6c28b86114645edf6b27c582bb2c54a5e2
Hugo Diniz <hhldiniz@gmail.com>
2021-09-30 20:07:41 -0300

    Adding class to hold types of content type

--- bin/controllers/serialized_data_controller.dart
+++ bin/controllers/serialized_data_controller.dart
@@ -1,9 +1,9 @@
-import 'dart:convert';
 import 'dart:mirrors';
 
 import 'package:shelf/shelf.dart';
 
 import '../annotations/serialization.dart';
+import '../enum/content_types.dart';
 import '../reflection/activator.dart';
 import 'base_controller.dart';
 
@@ -18,21 +18,38 @@ abstract class SerializedDataController<T> extends BaseController {
         .reflectee as T;
   }
 
+  Map<String, dynamic> _parseBodyData(
+      String rawBodyContent, ContentType contentType) {
+    print(rawBodyContent);
+    Map<String, dynamic> parsedResult = {};
+    if (contentType is FormData) {
+      var mainContentPattern = RegExp("name=\"\\w+\"\n+\\w+");
+      mainContentPattern.allMatches(rawBodyContent).forEach((match) {
+        print(match);
+      });
+    } else if (contentType is XWWFormUrlEncoded) {
+      
+    } else {}
+    return parsedResult;
+  }
+
   @override
   Future<Response> post(Request request) async {
+    var bodyContentFuture = request.readAsString();
+
     final InstanceMirror genericTypeMirror =
         reflect(Activator.createInstance(T));
 
-    for (var declaration
-        in genericTypeMirror.type.declarations.values.toList()) {
+    for (var declaration in genericTypeMirror.type.declarations.values) {
       if (declaration is VariableMirror) {
         try {
           var annotation = declaration.metadata
               .firstWhere((element) => element.reflectee is SerializedField)
               .reflectee as SerializedField;
           var fieldNameToSet = MirrorSystem.getName(declaration.simpleName);
-          Map<String, dynamic> mockedData =
-              jsonDecode("{\"$fieldNameToSet\": \"John\"}");
+          Map<String, dynamic> mockedData = _parseBodyData(
+              await bodyContentFuture,
+              ContentType.fromString(request.headers['content-type'] ?? ""));
           genericTypeMirror.setField(declaration.simpleName,
               mockedData[annotation.fieldName ?? fieldNameToSet]);
         } catch (e) {
