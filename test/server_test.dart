b1d20f1ff37083b36dd30c21b0c96d5f5dfa4ba2
Hugo Diniz <hhldiniz@gmail.com>
2021-10-16 18:46:43 -0300

    Running test server in pipeline

--- test/server_test.dart
+++ test/server_test.dart
@@ -1,29 +1,30 @@
+// import 'dart:io';
+
 import 'package:http/http.dart';
 import 'package:test/test.dart';
-import 'package:test_process/test_process.dart';
 
 void main() {
   final port = '8080';
   final host = 'http://0.0.0.0:$port';
-  TestProcess? serverProcess;
+  // Process? serverProcess;
 
-  setUp(() async {
-    serverProcess = await TestProcess.start(
+/*   setUp(() async {
+    serverProcess = await Process.start(
       'dart',
       ['reference/server.dart'],
       environment: {'PORT': port},
     );
-  });
+  }); */
 
   test('Hello World', () async {
-    addTearDown(() => serverProcess?.kill());
+    // addTearDown(() => serverProcess?.kill());
     final response = await get(Uri.parse(host + '/'));
     expect(response.statusCode, 200);
     expect(response.body, 'Hello World');
   });
 
   test("Serialize Data Test", () async {
-    addTearDown(() => serverProcess?.kill());
+    // addTearDown(() => serverProcess?.kill());
     var name = "John";
     var age = "20";
     final response = await post(Uri.parse(host + '/post_data'),
@@ -33,7 +34,7 @@ void main() {
   });
 
   test('404', () async {
-    addTearDown(() => serverProcess?.kill());
+    // addTearDown(() => serverProcess?.kill());
     final response = await get(Uri.parse(host + '/foobar'));
     expect(response.statusCode, 404);
   });
